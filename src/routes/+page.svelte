5e29a81a83b58d9d996ad7ee3e6be31450a33788
Deepfried Chips <33133709+Deepfried-Chips@users.noreply.github.com>
2022-09-07 09:23:30 +0300

    push changes to git, gotta distro change again fun.

---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+	import Header from "./common/header.svelte";
+</script>
+
+<svelte:head>
+	<title>Aegiscarr's Den</title>
+	<meta property="og:site_name" content="Aegiscarr">
+	<meta property="og:url" content="https://aegiscarr.com">
+	<meta property="og:title" content="Name of your page">
+	<meta property="og:description" content="A little description of this page">
+	<meta property="og:type" content="website">
+	<meta name="og:image" itemprop="image" content="static/img/wordmark-full.png">
+</svelte:head>
+
+<div class='page__inner'>
+	<Header/>
+</div>
+
+<style>
+    @import "$lib/css/theme.css";
+</style>
---
+++ src/routes/common/header.svelte
@@ -0,0 +1,10 @@
+<script>
+	import Bunger from "./bunger.svelte";
+
+	let visible = false;
+</script>
+
+<div class='header'>
+	<button on:click={() => {visible = !visible}}>Toggle</button>
+</div>
+<Bunger visible={visible}/>
