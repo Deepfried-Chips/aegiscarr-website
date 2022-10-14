bfcfc469b6947109cd036ef6a11019c87f509664
Deepfried Chips <33133709+Deepfried-Chips@users.noreply.github.com>
2022-10-14 11:27:12 +0300

    rebuilt with tailwind set up, typescript, set up static adapter, and go insane.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,2 @@
-<script>
-	import Header from "./common/header.svelte";
-</script>
-
-<svelte:head>
-	<title>Aegiscarr's Den</title>
-	<meta property="og:site_name" content="Aegiscarr">
-	<meta property="og:url" content="https://aegiscarr.com">
-	<meta property="og:title" content="Name of your page">
-	<meta property="og:description" content="A little description of this page">
-	<meta property="og:type" content="website">
-	<meta name="og:image" itemprop="image" content="static/img/wordmark-full.png">
-</svelte:head>
-
-<div class='page__inner'>
-	<Header/>
-</div>
-
-<style>
-    @import "$lib/css/theme.css";
-</style>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
